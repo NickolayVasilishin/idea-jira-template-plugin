e9e52a98934f1862868e8e7a0d26555b1523ce12
GitHub Action <action@github.com>
2020-12-15 15:23:09 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/nickolayvasilishin/ideajiratemplateplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.nickolayvasilishin.ideajiratemplateplugin.services
+
+import com.github.nickolayvasilishin.ideajiratemplateplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
